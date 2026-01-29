1e40cd662135253494530fd2ae264dd938b614ec
ThinhPhoenix <laithinh1310@gmail.com>
2026-01-29 16:53:06 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,7 @@ export class Browser {
                 if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
                 window.open(`x-safari-${url}`)
                 } else {
-                    const urlObj = new URL(url);
-                    const scheme = urlObj.protocol.replace(/:$/, "");
-                    const intentUrl = `intent://${urlObj.host}${urlObj.pathname}${urlObj.search}#Intent;scheme=${scheme};action=android.intent.action.VIEW;S.browser_fallback_url=${encodeURIComponent(url)};end`;
-                    const link = document.createElement('a');
-                    link.href = intentUrl;
-                    link.target = '_blank';
-                    document.body.appendChild(link);
-                    link.click();
-                    document.body.removeChild(link);
+                   window.open(`goto://DeviceBrowser?url=${url}`)
                 }
             } else {
                 window.open(url, '_blank')
